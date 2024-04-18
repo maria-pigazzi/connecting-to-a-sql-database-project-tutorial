dac579da8e00a9f98d807fe668e2b610734baaad
Maria Pigazzi <83117801+maria-pigazzi@users.noreply.github.com>
2024-04-18 19:08:35 +0000

    finish project

--- src/app.py
+++ src/app.py
@@ -20,11 +20,47 @@ def connect():
 con = connect()
 # 2) Execute the SQL sentences () to create your tables using the SQLAlchemy's execute function
 
-# eliminated the con.execute of the tables otherwise it was going to make problems executing itself more times in the terminal
+# commented the con.execute of the tables otherwise it was going to make problems executing itself more times in the terminal
+con.execute("""
+CREATE TABLE publishers(
+    publisher_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(publisher_id)
+);
 
+CREATE TABLE authors(
+    author_id INT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(50) NULL,
+    last_name VARCHAR(100) NULL,
+    PRIMARY KEY(author_id)
+);
+
+CREATE TABLE books(
+    book_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    total_pages INT NULL,
+    rating DECIMAL(4, 2) NULL,
+    isbn VARCHAR(13) NULL,
+    published_date DATE,
+    publisher_id INT NULL,
+    PRIMARY KEY(book_id),
+    CONSTRAINT fk_publisher FOREIGN KEY(publisher_id) REFERENCES publishers(publisher_id)
+);
+
+CREATE TABLE book_authors (
+    book_id INT NOT NULL,
+    author_id INT NOT NULL,
+    PRIMARY KEY(book_id, author_id),
+    CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
+);
+""")
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-insert_data = ("""
+# insert_data = ("""...)
+
+con.execute("""
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
